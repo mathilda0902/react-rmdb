7b67d6efed3b158de254a1f7da5971578003cf7d
mathilda0902 <vicky.kwan@mapbox.com>
2020-09-05 11:52:56 -0700

    added movie thumbs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
     API_URL,
     API_KEY,
+    API_BASE_URL,
+    POSTER_SIZE,
     IMAGE_BASE_URL,
     BACKDROP_SIZE,
-    POSTER_SIZE
 } from '../config';
 
 // import components
@@ -17,24 +18,55 @@ import Spinner from './elements/Spinner';
 
 // Custom Hook
 import { useHomeFetch } from './hooks/useHomeFetch';
+// no image holder
+import NoImage from './images/no_image.jpg';
 
 const Home = () => {
-    const [{ state, loading, error}, fetchMovies] = useHomeFetch();
-    console.log(state);
+    const [
+        {   
+            state: { movies, currentPage, totalPages, heroImage }, 
+            loading, 
+            error,
+        }, 
+        fetchMovies
+    ] = useHomeFetch();
+    // console.log(state);
+
+    const [searchTerm, setSearchTerm] = useState('');
     
     if (error) return <div>Something went wrong ...</div>;
-    if (!state.movies[0]) return <Spinner/>;
+    // if (!state.movies[0]) return <Spinner/>;
+    // multi-level deconstruct
+    if (!movies[0]) return <Spinner/>;
 
     return (
         // empty tags equals: React.fragments tag
         <>
         <HeroImage 
-            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
-            title={state.heroImage.original_title}
-            text={state.heroImage.overview}
+            // image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+            // title={state.heroImage.original_title}
+            title={heroImage.original_title}
+            // text={state.heroImage.overview}
+            text={heroImage.overview}
         />
         <SearchBar />
-        <Grid />
+        <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
+            {/* {state.movies.map(movie => ( */}
+            {movies.map(movie => (
+                <MovieThumb 
+                    key={movie.id}
+                    clickable
+                    image={
+                        movie.poster_path 
+                            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                            : NoImage
+                    }
+                    movieId={movie.id}
+                    movieName={movie.original_title}
+                />
+        ))}
+        </Grid> 
         <MovieThumb />
         <Spinner />
         <LoadMoreBtn />
