a333ea1721f56389e45507b06504a03fd6b47917
mathilda0902 <vicky.kwan@mapbox.com>
2020-09-12 15:08:53 -0700

    added links and fetch data hooks

---
+++ src/components/elements/Actor.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Actor = () => <div>actor</div>
+
+export default Actor;
---
+++ src/components/elements/Navigation.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Navigation = () => <div>Nav</div>
+
+export default Navigation;
